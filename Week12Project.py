b837c84d56d7ac20f0026fb52d5b4bd8bb31acc7
Todd Caputo <tcaputo001@my.wilmu.edu>
2022-07-26 22:55:44 +0000

    Week 12 Project Committed

---
+++ Week12Project.py
@@ -0,0 +1,19 @@
+# This is the start of my Week 12 Project
+my_aws_list = []
+print(my_aws_list)
+my_aws_list.append("S3")
+print(my_aws_list)
+my_aws_list.append("Cloud Formation")
+my_aws_list.append("VPC")
+my_aws_list.append("EC2")
+my_aws_list.append("Cloud9")
+print(my_aws_list)
+print(len(my_aws_list))
+my_aws_list.remove("S3")
+print(my_aws_list)
+print(len(my_aws_list))
+del my_aws_list[3]
+print(my_aws_list)
+
+
+
