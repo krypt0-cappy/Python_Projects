e23adb57fb210d7aaa45005970bed1564e359b75
Todd Caputo <tcaputo001@my.wilmu.edu>
2022-07-27 00:26:07 +0000

    2nd commit

--- Week12Project.py
+++ Week12Project.py
@@ -1,19 +1,44 @@
 # This is the start of my Week 12 Project
+# Creating an empty list to fill
 my_aws_list = []
-print(my_aws_list)
+
+# Let's take our text string and make a variable to use later
+List_Text = "This is a list of my favorite AWS Services so far: "
+print(List_Text)
+
+# Let's take our item count string and make a variable to use later
+Item_Count_Text = "My list contains the following number of items: "
+print(Item_Count_Text, len(my_aws_list), "\n")
+
+# Using the append element to add a few items to our list
 my_aws_list.append("S3")
-print(my_aws_list)
 my_aws_list.append("Cloud Formation")
 my_aws_list.append("VPC")
-my_aws_list.append("EC2")
-my_aws_list.append("Cloud9")
-print(my_aws_list)
-print(len(my_aws_list))
+my_aws_list.append("CodePipeline")
+print(List_Text, my_aws_list)
+
+# Print the lenth of the current list using the len parameter
+print(Item_Count_Text, len(my_aws_list), "\n")
+
+# Using the insert element to add a few items to our list in a specific order
+my_aws_list.insert(3, "EC2")
+my_aws_list.insert(4, "Cloud9")
+my_aws_list.insert(5, "CodeBuild")
+my_aws_list.insert(6, "RDS")
+print(List_Text, my_aws_list)
+print(Item_Count_Text, len(my_aws_list), "\n")
+
+# Removing an item from the list using the remove parameter
 my_aws_list.remove("S3")
-print(my_aws_list)
-print(len(my_aws_list))
+print(List_Text, my_aws_list)
+print(Item_Count_Text, len(my_aws_list), "\n")
+
+# Removing an item from the list using the del parameter
 del my_aws_list[3]
-print(my_aws_list)
 
+# Print the final list and current list count
+print(List_Text, my_aws_list)
+print(Item_Count_Text, len(my_aws_list), "\n")
 
+print("Welcome to the end of our Week 12 Python Project!")
 
