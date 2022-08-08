79e1b55383921e698bc963c8a66146894f57a4c0
Todd Caputo <tcaputo001@my.wilmu.edu>
2022-08-08 21:42:47 +0000

    environment instances code committed

--- stop_environment_instances.py
+++ stop_environment_instances.py
@@ -5,7 +5,7 @@ import boto3
 ec2 = boto3.resource('ec2', region_name='us-east-1')
 
 def stopped_environment_instances(ec2):
-    running_environment_instnaces = ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']},{'Name': 'tag:Environment','Values':['Dev']}])
+    running_environment_instances = ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']},{'Name': 'tag:Environment','Values':['Dev']}])
     for instance in running_environment_instances:
         id=instance.id
         ec2.instances.filter(InstanceIds=[id]).start()
